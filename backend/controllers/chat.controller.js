9e3a8062d82ba01175091d6f97c1040216484a52
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-08-10 22:36:13 +0530

    make routes and contolles of chat and message and make add chat api

---
+++ backend/controllers/chat.controller.js
@@ -0,0 +1,37 @@
+import prisma from "../lib/prisma.js";
+
+export const getChats = async (req, res) => {
+  const tokenUserId = req.userId;
+  try {
+    const chats = await prisma.chat.findMany({
+      where: {
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+      },
+    });
+    res.status(200).json(chats);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Failed to get users" });
+  }
+};
+
+export const getChat = async (req, res) => {};
+
+export const addChat = async (req, res) => {
+  const tokenUserId = req.userId;
+  try {
+    const newChat = await prisma.chat.create({
+      data: {
+        userIDs: [tokenUserId, req.body.receiverId],
+      },
+    });
+    res.status(200).json(newChat);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to add chat!" });
+  }
+};
+
+export const readChat = async (req, res) => {};
---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,5 @@
+import prisma from "../lib/prisma.js";
+
+export const addMessage = async (req, res) => {
+
+};
