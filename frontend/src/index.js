b497041b89e11b0ba6b25290a79d54062f26ada5
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-08-16 13:02:42 +0530

    make another context for socket and folder for socket and install the prior dependencies and libraries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
+import { SocketContextProvider } from "./context/SocketContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <SocketContextProvider>
+        <App />
+      </SocketContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
